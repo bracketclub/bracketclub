17d18d9e3daca0744280dda944099f52135484e4
Luke Karrys <lukekarrys@gmail.com>
2016-08-09 11:38:44 -0700

    lint

--- packages/bracket-generator/index.js
+++ packages/bracket-generator/index.js
@@ -1,165 +1,165 @@
-var bracketData = require('bracket-data');
-var _isString = require('lodash/isString');
-var _map = require('lodash/map');
-var _random = require('lodash/random');
-var _uniq = require('lodash/uniq');
-var _toArray = require('lodash/toArray');
-var _difference = require('lodash/difference');
-var _flatten = require('lodash/flatten');
-var _last = require('lodash/last');
-var _find = require('lodash/find');
-var _defaults = require('lodash/defaults');
-var _extend = require('lodash/assign');
-var _filter = require('lodash/filter');
-var _omit = require('lodash/omit');
-var _pick = require('lodash/pick');
+var bracketData = require('bracket-data')
+var _isString = require('lodash/isString')
+var _map = require('lodash/map')
+var _random = require('lodash/random')
+var _uniq = require('lodash/uniq')
+var _toArray = require('lodash/toArray')
+var _difference = require('lodash/difference')
+var _flatten = require('lodash/flatten')
+var _last = require('lodash/last')
+var _find = require('lodash/find')
+var _defaults = require('lodash/defaults')
+var _extend = require('lodash/assign')
+var _filter = require('lodash/filter')
+var _omit = require('lodash/omit')
+var _pick = require('lodash/pick')
 
-function Generator(options) {
-    this.bracketData = bracketData({
-        year: options.year,
-        sport: options.sport
-    });
+function Generator (options) {
+  this.bracketData = bracketData({
+    year: options.year,
+    sport: options.sport
+  })
 
-    return this.reset(_omit(options, 'sport', 'year'));
+  return this.reset(_omit(options, 'sport', 'year'))
 }
 
 Generator.prototype.reset = function (options) {
-    _defaults(options, {
-        winners: '',
-        generatedBracket: null,
-        winnerCounter: 0,
-        regionCounter: 0,
-        finishedRegions: null
-    });
+  _defaults(options, {
+    winners: '',
+    generatedBracket: null,
+    winnerCounter: 0,
+    regionCounter: 0,
+    finishedRegions: null
+  })
 
-    _extend(this, _pick(options, 'winnerCounter', 'regionCounter', 'generatedBracket', 'finishedRegions'));
-    this.options = _pick(options, 'winners');
+  _extend(this, _pick(options, 'winnerCounter', 'regionCounter', 'generatedBracket', 'finishedRegions'))
+  this.options = _pick(options, 'winners')
 
-    return this;
-};
+  return this
+}
 
 Generator.prototype.generateWinner = function (matchup) {
   var self = this
 
-    if (_isString(matchup[0]) && _isString(matchup[1])) {
-        matchup = _map(matchup, function (region) {
-            return self.winningTeamFromRegion(region);
-        });
+  if (_isString(matchup[0]) && _isString(matchup[1])) {
+    matchup = _map(matchup, function (region) {
+      return self.winningTeamFromRegion(region)
+    })
+  }
+
+  var possible = {
+    random: _random(matchup.length - 1),
+          // Higher means higher seed OR if seeds are the same 2nd team
+    higher: function () {
+      if (_uniq(matchup, true).length < 2) return 1
+      return matchup.indexOf(Math.max.apply(Math, matchup))
+    },
+          // Lower means lower seed OR if seeds are the same 1st team
+    lower: function () {
+      if (_uniq(matchup, true).length < 2) return 0
+      return matchup.indexOf(Math.min.apply(Math, matchup))
     }
+  }
+  var pickIndex = this.options.winners.length >= this.bracketData.order.length
+    ? this.regionCounter * (this.bracketData.order.length - 1) + (this.winnerCounter + 1) - 1
+    : this.winnerCounter
+  var pick = this.options.winners.charAt(pickIndex)
+  var winner
 
-    var possible = {
-            random: _random(matchup.length - 1),
-            // Higher means higher seed OR if seeds are the same 2nd team
-            higher: function () {
-                if (_uniq(matchup, true).length < 2) return 1;
-                return matchup.indexOf(Math.max.apply(Math, matchup));
-            },
-            // Lower means lower seed OR if seeds are the same 1st team
-            lower: function () {
-                if (_uniq(matchup, true).length < 2) return 0;
-                return matchup.indexOf(Math.min.apply(Math, matchup));
-            }
-        },
-        pickIndex = this.options.winners.length >= this.bracketData.order.length ?
-            this.regionCounter * (this.bracketData.order.length - 1) + (this.winnerCounter + 1) - 1 :
-            this.winnerCounter,
-        pick = this.options.winners.charAt(pickIndex),
-        winner;
+  if (pick === '1') {
+    winner = possible.higher()
+  } else if (pick === '0') {
+    winner = possible.lower()
+  } else if (typeof possible[this.options.winners] === 'function') {
+    winner = possible[this.options.winners]()
+  } else if (typeof possible[this.options.winners] !== 'undefined') {
+    winner = possible[this.options.winners]
+  }
 
-    if (pick === '1') {
-        winner = possible.higher();
-    } else if (pick === '0') {
-        winner = possible.lower();
-    } else if (typeof possible[this.options.winners] === 'function') {
-        winner = possible[this.options.winners]();
-    } else if (typeof possible[this.options.winners] !== 'undefined') {
-        winner = possible[this.options.winners];
-    }
-
-    return (winner >= 0 && winner < matchup.length) ? winner : possible.random;
-};
+  return (winner >= 0 && winner < matchup.length) ? winner : possible.random
+}
 
 Generator.prototype.generateRound = function (opts) {
-    var seeds = opts.seeds,
-        matchup = [seeds[0], seeds[1]],
-        winner = matchup[this.generateWinner(matchup)],
-        winners = (opts.winners || []).concat(winner),
-        remainingSeeds = seeds.splice(2);
+  var seeds = opts.seeds
+  var matchup = [seeds[0], seeds[1]]
+  var winner = matchup[this.generateWinner(matchup)]
+  var winners = (opts.winners || []).concat(winner)
+  var remainingSeeds = seeds.splice(2)
 
-    this.winnerCounter++;
+  this.winnerCounter++
 
-    if (remainingSeeds.length === 0) {
-        return winners;
-    } else {
-        return this.generateRound({seeds: remainingSeeds, winners: winners});
-    }
-};
+  if (remainingSeeds.length === 0) {
+    return winners
+  } else {
+    return this.generateRound({seeds: remainingSeeds, winners: winners})
+  }
+}
 
 Generator.prototype.generateRounds = function (opts) {
-    var optRound = _toArray(opts.round),
-        round = this.generateRound({seeds: opts.round}),
-        rounds = (opts.rounds || []);
+  var optRound = _toArray(opts.round)
+  var round = this.generateRound({seeds: opts.round})
+  var rounds = (opts.rounds || [])
 
-    if (rounds.length === 0) {
-        rounds.push(optRound);
-    }
-    rounds.push(_toArray(round));
+  if (rounds.length === 0) {
+    rounds.push(optRound)
+  }
+  rounds.push(_toArray(round))
 
-    if (round.length === 1) {
-        this.regionCounter++;
-        return rounds;
-    } else {
-        return this.generateRounds({round: round,  rounds: rounds});
-    }
-};
+  if (round.length === 1) {
+    this.regionCounter++
+    return rounds
+  } else {
+    return this.generateRounds({round: round, rounds: rounds})
+  }
+}
 
 Generator.prototype.generateRegion = function (region) {
-    this.winnerCounter = 0;
-    return {id: region.id, rounds: this.generateRounds({round: this.bracketData.order.slice()})};
-};
+  this.winnerCounter = 0
+  return {id: region.id, rounds: this.generateRounds({round: this.bracketData.order.slice()})}
+}
 
 Generator.prototype.generateRegions = function () {
-    this.regionCounter = 0;
-    var regions = _map(_filter(this.bracketData.bracket.regions, function (r) { return !!r.teams; }), this.generateRegion.bind(this));
-    this.finishedRegions = regions;
-    return regions;
-};
+  this.regionCounter = 0
+  var regions = _map(_filter(this.bracketData.bracket.regions, function (r) { return !!r.teams }), this.generateRegion.bind(this))
+  this.finishedRegions = regions
+  return regions
+}
 
 Generator.prototype.generateBracket = function () {
-    if (this.generatedBracket === null) {
-        this.generatedBracket = this.generateRegions().concat(this.generateFinal());
-    }
-    return this.generatedBracket;
-};
+  if (this.generatedBracket === null) {
+    this.generatedBracket = this.generateRegions().concat(this.generateFinal())
+  }
+  return this.generatedBracket
+}
 
 Generator.prototype.generateFinalFour = function () {
-    var regions = this.bracketData.constants.REGION_IDS,
-        firstTeam = regions[0],
-        matchup1 = [firstTeam, this.bracketData.bracket.regions[firstTeam].sameSideAs],
-        matchup2 = _difference(regions, matchup1);
-    return _flatten([matchup1, matchup2]);
-};
+  var regions = this.bracketData.constants.REGION_IDS
+  var firstTeam = regions[0]
+  var matchup1 = [firstTeam, this.bracketData.bracket.regions[firstTeam].sameSideAs]
+  var matchup2 = _difference(regions, matchup1)
+  return _flatten([matchup1, matchup2])
+}
 
 Generator.prototype.generateFinal = function () {
-    this.winnerCounter = 0;
-    return {id: this.bracketData.constants.FINAL_ID, name: this.bracketData.constants.FINAL_NAME, rounds: this.generateRounds({round: this.generateFinalFour()})};
-};
+  this.winnerCounter = 0
+  return {id: this.bracketData.constants.FINAL_ID, name: this.bracketData.constants.FINAL_NAME, rounds: this.generateRounds({round: this.generateFinalFour()})}
+}
 
 Generator.prototype.winningTeamFromRegion = function (fromRegion) {
-    var hasFinishedRegions = !!(this.finishedRegions.length),
-        regions = (hasFinishedRegions) ? this.finishedRegions : this.generateBracket();
-    return _last(_find(regions, function (region) {
-        return region.id === fromRegion;
-    }).rounds)[0];
-};
+  var hasFinishedRegions = !!(this.finishedRegions.length)
+  var regions = (hasFinishedRegions) ? this.finishedRegions : this.generateBracket()
+  return _last(_find(regions, function (region) {
+    return region.id === fromRegion
+  }).rounds)[0]
+}
 
 Generator.prototype.generate = function (winners) {
-    winners && this.reset({winners: winners});
-    return _map(_flatten(_toArray(this.generateBracket())), function (region) {
-        return region.id + _flatten(region.rounds).join('');
-    }).join('')
+  winners && this.reset({winners: winners})
+  return _map(_flatten(_toArray(this.generateBracket())), function (region) {
+    return region.id + _flatten(region.rounds).join('')
+  }).join('')
     .replace(new RegExp(this.bracketData.order.join(''), 'g'), '')
-    .replace(new RegExp(this.generateFinalFour().join(''), 'g'), '');
-};
+    .replace(new RegExp(this.generateFinalFour().join(''), 'g'), '')
+}
 
-module.exports = Generator;
+module.exports = Generator
--- packages/bracket-generator/test/test.js
+++ packages/bracket-generator/test/test.js
@@ -1,118 +1,120 @@
-var BracketGenerator = require('../index');
-var assert = require('assert');
-var year = '2013';
-var sport = 'ncaam';
-var chalk = 'MW185463721432121W185463721432121S185463721432121E185463721432121FFMWSMW';
-var upsets = 'MW16912131114101516131415161516W16912131114101516131415161516S16912131114101516131415161516E16912131114101516131415161516FFWEE';
-var BracketValidator = require('bracket-validator');
-var bd = require('bracket-data');
-var bracketData = bd({year: year, sport: sport});
-var finalGames = bracketData.constants.REGION_COUNT - 1;
-var firstRoundGames = bracketData.order.length - 1;
-var _ = require('lodash');
+/* eslint-env mocha */
+
+var BracketGenerator = require('../index')
+var assert = require('assert')
+var year = '2013'
+var sport = 'ncaam'
+var chalk = 'MW185463721432121W185463721432121S185463721432121E185463721432121FFMWSMW'
+var upsets = 'MW16912131114101516131415161516W16912131114101516131415161516S16912131114101516131415161516E16912131114101516131415161516FFWEE'
+var BracketValidator = require('bracket-validator')
+var bd = require('bracket-data')
+var bracketData = bd({year: year, sport: sport})
+var finalGames = bracketData.constants.REGION_COUNT - 1
+var firstRoundGames = bracketData.order.length - 1
+var _ = require('lodash')
 
 var intToBinary = function (i, length) {
-    typeof i === 'undefined' && (i = _.random(0, Math.pow(2, length)));
-    return _.padStart((i).toString(2), length, '0');
-};
+  typeof i === 'undefined' && (i = _.random(0, Math.pow(2, length)))
+  return _.padStart((i).toString(2), length, '0')
+}
 
-var intToRegionBinary = _.partialRight(intToBinary, firstRoundGames);
-var intToFinalBinary = _.partialRight(intToBinary, finalGames);
+var intToRegionBinary = _.partialRight(intToBinary, firstRoundGames)
+var intToFinalBinary = _.partialRight(intToBinary, finalGames)
 
 // Creates a bracket with one defined region and everything else random
 var fullBracketBinary = function (i) {
-    return [intToRegionBinary(i), intToRegionBinary(), intToRegionBinary(), intToRegionBinary(), intToFinalBinary()].join('');
-};
+  return [intToRegionBinary(i), intToRegionBinary(), intToRegionBinary(), intToRegionBinary(), intToFinalBinary()].join('')
+}
 
 describe('Bracket generator', function () {
-    it('generates a flat string bracket', function () {
-        var generator = new BracketGenerator({year: year, sport: sport});
-        assert.equal(true, typeof generator.generate() === 'string');
-    });
+  it('generates a flat string bracket', function () {
+    var generator = new BracketGenerator({year: year, sport: sport})
+    assert.equal(true, typeof generator.generate() === 'string')
+  })
 
-    it('generates an all lower seeded bracket', function () {
-        var generator = new BracketGenerator({year: year, sport: sport, winners: 'lower'});
-        assert.equal(generator.generate(), chalk);
-    });
+  it('generates an all lower seeded bracket', function () {
+    var generator = new BracketGenerator({year: year, sport: sport, winners: 'lower'})
+    assert.equal(generator.generate(), chalk)
+  })
 
-    it('generates an all higher seeded bracket', function () {
-        var generator = new BracketGenerator({year: year, sport: sport, winners: 'higher'});
-        assert.equal(generator.generate(), upsets);
-    });
+  it('generates an all higher seeded bracket', function () {
+    var generator = new BracketGenerator({year: year, sport: sport, winners: 'higher'})
+    assert.equal(generator.generate(), upsets)
+  })
 
-    it('generates a bracket from a binary string', function () {
-        var generator = new BracketGenerator({year: year, sport: sport, winners: fullBracketBinary(4044)});
-        var bracket = generator.generate();
+  it('generates a bracket from a binary string', function () {
+    var generator = new BracketGenerator({year: year, sport: sport, winners: fullBracketBinary(4044)})
+    var bracket = generator.generate()
+    var validator = new BracketValidator({
+      year: year,
+      sport: sport,
+      testOnly: true,
+      allowEmpty: false,
+      flatBracket: bracket
+    })
+    var validated = validator.validate()
+    assert.equal(false, validated instanceof Error)
+  })
+
+  it('Can be reset', function () {
+    var generator = new BracketGenerator({year: year, sport: sport})
+    assert.equal(generator.generate('higher'), upsets)
+    assert.equal(generator.generate('lower'), chalk)
+    assert.notEqual(generator.generate('random'), chalk)
+    assert.notEqual(generator.generate('random'), upsets)
+  })
+
+  it('Can be validated', function () {
+    var generator = new BracketGenerator({year: year, sport: sport})
+    var validator = new BracketValidator({
+      year: year,
+      sport: sport,
+      testOnly: true,
+      allowEmpty: false
+    })
+    assert.equal(false, validator.validate(generator.generate('lower')) instanceof Error)
+    assert.equal(false, validator.validate(generator.generate('higher')) instanceof Error)
+  })
+
+  it('Random binary brackets can be validated', function () {
+    var iterations = 2000
+    var start = Math.max(0, Math.pow(2, firstRoundGames) - iterations)
+    var stop = start + iterations
+    for (var i = start, m = stop; i < m; i++) {
+      (function (i) {
+        var generator = new BracketGenerator({year: year, sport: sport, winners: fullBracketBinary(i)})
+        var bracket = generator.generate()
         var validator = new BracketValidator({
-            year: year,
-            sport: sport,
-            testOnly: true,
-            allowEmpty: false,
-            flatBracket: bracket
-        });
-        var validated = validator.validate();
-        assert.equal(false, validated instanceof Error);
-    });
+          year: year,
+          sport: sport,
+          testOnly: true,
+          allowEmpty: false,
+          flatBracket: bracket
+        })
+        var validated = validator.validate()
+        assert.equal(false, validated instanceof Error)
+      })(i)
+    }
+  })
 
-    it('Can be reset', function () {
-        var generator = new BracketGenerator({year: year, sport: sport});
-        assert.equal(generator.generate('higher'), upsets);
-        assert.equal(generator.generate('lower'), chalk);
-        assert.notEqual(generator.generate('random'), chalk);
-        assert.notEqual(generator.generate('random'), upsets);
-    });
-
-    it('Can be validated', function () {
-        var generator = new BracketGenerator({year: year, sport: sport});
+  it('Random brackets can be validated', function () {
+    var iterations = 2000
+    var start = Math.max(0, Math.pow(2, firstRoundGames) - iterations)
+    var stop = start + iterations
+    for (var i = start, m = stop; i < m; i++) {
+      (function () {
+        var generator = new BracketGenerator({year: year, sport: sport, winners: 'random'})
+        var bracket = generator.generate()
         var validator = new BracketValidator({
-            year: year,
-            sport: sport,
-            testOnly: true,
-            allowEmpty: false
-        });
-        assert.equal(false, validator.validate(generator.generate('lower')) instanceof Error);
-        assert.equal(false, validator.validate(generator.generate('higher')) instanceof Error);
-    });
-
-    it('Random binary brackets can be validated', function () {
-        var iterations = 2000;
-        var start = Math.max(0, Math.pow(2, firstRoundGames) - iterations);
-        var stop = start + iterations;
-        for (var i = start, m = stop; i < m; i++) {
-            (function (i) {
-                var generator = new BracketGenerator({year: year, sport: sport, winners: fullBracketBinary(i)});
-                var bracket = generator.generate();
-                var validator = new BracketValidator({
-                    year: year,
-                    sport: sport,
-                    testOnly: true,
-                    allowEmpty: false,
-                    flatBracket: bracket
-                });
-                var validated = validator.validate();
-                assert.equal(false, validated instanceof Error);
-            })(i);
-        }
-    });
-
-    it('Random brackets can be validated', function () {
-        var iterations = 2000;
-        var start = Math.max(0, Math.pow(2, firstRoundGames) - iterations);
-        var stop = start + iterations;
-        for (var i = start, m = stop; i < m; i++) {
-            (function () {
-                var generator = new BracketGenerator({year: year, sport: sport, winners: 'random'});
-                var bracket = generator.generate();
-                var validator = new BracketValidator({
-                    year: year,
-                    sport: sport,
-                    testOnly: true,
-                    allowEmpty: false,
-                    flatBracket: bracket
-                });
-                var validated = validator.validate();
-                assert.equal(false, validated instanceof Error);
-            })(i);
-        }
-    });
-});
+          year: year,
+          sport: sport,
+          testOnly: true,
+          allowEmpty: false,
+          flatBracket: bracket
+        })
+        var validated = validator.validate()
+        assert.equal(false, validated instanceof Error)
+      })(i)
+    }
+  })
+})
