f7990ebcae608cc3d6616bc24b51ff3a57f66a07
Luke Karrys <lukekarrys@gmail.com>
2016-12-15 11:29:01 -0800

    move stuff around

---
+++ packages/bracket-insights/bin/fetch-data.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+API=https://tyb-api.now.sh/
+MEN=ncaam
+WOMEN=ncaaw
+ENTRIES=entries
+MASTERS=masters
+DATA_DIR=".data"
+
+curl -s ${API}${ENTRIES}/${MEN}-2012 > ${DATA_DIR}/${ENTRIES}-${MEN}-2012.json
+curl -s ${API}${ENTRIES}/${MEN}-2013 > ${DATA_DIR}/${ENTRIES}-${MEN}-2013.json
+curl -s ${API}${ENTRIES}/${MEN}-2014 > ${DATA_DIR}/${ENTRIES}-${MEN}-2014.json
+curl -s ${API}${ENTRIES}/${MEN}-2015 > ${DATA_DIR}/${ENTRIES}-${MEN}-2015.json
+curl -s ${API}${ENTRIES}/${MEN}-2016 > ${DATA_DIR}/${ENTRIES}-${MEN}-2016.json
+curl -s ${API}${ENTRIES}/${MEN}-2017 > ${DATA_DIR}/${ENTRIES}-${MEN}-2017.json
+
+curl -s ${API}${ENTRIES}/${WOMEN}-2016 > ${DATA_DIR}/${ENTRIES}-${WOMEN}-2016.json
+curl -s ${API}${ENTRIES}/${WOMEN}-2017 > ${DATA_DIR}/${ENTRIES}-${WOMEN}-2017.json
+
+curl -s ${API}${MASTERS}/${MEN}-2012 > ${DATA_DIR}/${MASTERS}-${MEN}-2012.json
+curl -s ${API}${MASTERS}/${MEN}-2013 > ${DATA_DIR}/${MASTERS}-${MEN}-2013.json
+curl -s ${API}${MASTERS}/${MEN}-2014 > ${DATA_DIR}/${MASTERS}-${MEN}-2014.json
+curl -s ${API}${MASTERS}/${MEN}-2015 > ${DATA_DIR}/${MASTERS}-${MEN}-2015.json
+curl -s ${API}${MASTERS}/${MEN}-2016 > ${DATA_DIR}/${MASTERS}-${MEN}-2016.json
+curl -s ${API}${MASTERS}/${MEN}-2017 > ${DATA_DIR}/${MASTERS}-${MEN}-2017.json
+
+curl -s ${API}${MASTERS}/${WOMEN}-2016 > ${DATA_DIR}/${MASTERS}-${WOMEN}-2016.json
+curl -s ${API}${MASTERS}/${WOMEN}-2017 > ${DATA_DIR}/${MASTERS}-${WOMEN}-2017.json
