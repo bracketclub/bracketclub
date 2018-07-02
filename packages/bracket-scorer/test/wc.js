6945605e345ebf7874ce645fe36c1a3b9da7a66a
Luke Karrys <lukekarrys@gmail.com>
2018-07-02 08:39:19 -0700

    world cup ppr add test

--- packages/bracket-scorer/test/wc.js
+++ packages/bracket-scorer/test/wc.js
@@ -5,10 +5,9 @@ var _ = require('lodash')
 var BracketScorer = require('../index')
 
 describe('World Cup', function () {
-  var sport = 'wcm'
-  var year = '2018'
-
   it('should diff with best of', function () {
+    var sport = 'wcm'
+    var year = '2018'
     var entry = 'L11422331112213R11422331112211FL1'
     var master = 'L11422331112213R11422331112211FL1'
     var diff = new BracketScorer({
@@ -28,5 +27,30 @@ describe('World Cup', function () {
         }
       })
     })
+  })
+
+  it('should have the same ppr', function () {
+    var sport = 'wcm'
+    var year = '2018'
+    var entry1 = 'L8252233251222R12412161136212FL2'
+    var entry2 = 'L83522131513331R11412162116113FL2'
+    var master = 'LXXXXXXXRXXXXXXXFX'
+
+    var score1 = new BracketScorer({
+      entry: entry1,
+      master: master,
+      sport: sport,
+      year: year
+    }).score(['standardPPR'])
+
+    var score2 = new BracketScorer({
+      entry: entry2,
+      master: master,
+      sport: sport,
+      year: year
+    }).score(['standardPPR'])
+
+    assert.equal(score1, 480)
+    assert.equal(score2, 492)
   })
 })
